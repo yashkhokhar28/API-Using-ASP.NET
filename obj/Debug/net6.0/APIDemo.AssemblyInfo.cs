cfdf8129418ef22ac6e0ad76387e7a47bbdf8f82
yashkhokhar28 <yashkhokhar28@gmail.com>
2024-01-04 19:38:16 +0530

    minor changes

--- obj/Debug/net6.0/APIDemo.AssemblyInfo.cs
+++ obj/Debug/net6.0/APIDemo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APIDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc80e3da29501938c2e0525b9832cc2b196db9d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("APIDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APIDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
